f859157949fd483a49ad66ed7d0756bf4434be52
grglgn <grglgn@inbox.ru>
2019-09-24 22:29:24 +0300

    сделал календарь на ui-jquery

--- src/main/web/raspisanie-slu.js
+++ src/main/web/raspisanie-slu.js
@@ -1,7 +1,24 @@
-function loadUI(){
- $("#startDtSpan").append(newCalendar("startDt"));
- $("#endDtSpan").append(newCalendar("endDt"));
-}
+$(function (){
+// $("#startDtInp").append(newCalendar("startDt"));
+// $("#endDtSpan").append(newCalendar("endDt"));
+var clConf = {
+dateFormat: 'dd-mm-yy',
+monthNames:["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],
+monthNamesShort:["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"],
+dayNamesMin:["Пн","Вт","Ср","Чт","Пт","Сб","Вс"],
+dayNames:["Пн","Вт","Ср","Чт","Пт","Сб","Вс"],
+changeYear:true,
+changeMonth:true
+
+};
+//$.datepicker.setDefaults( $.datepicker.regional[ "ru" ] );
+$("#startDtInp" ).datepicker(clConf);
+$("#endDtInp" ).datepicker(clConf);
+$("#loadBtn").button();
+
+//$("#startDtInp" ).datepicker.formatDate( "dd-mm-yy", new Date());
+//$("#endDtInp" ).datepicker.formatDate( "dd-mm-yy", new Date());
+});
 
 function newCalendar(id){
     var cl  = $("<select id='clD-"+id+"' class='clDay'></select><select id='clM-"+id+"' class='clMont'></select><select id='clY-"+id+"' class='clYear'></select>");
